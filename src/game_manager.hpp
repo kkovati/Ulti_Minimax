c21fe7f4e00046af5c1f0806ccbad52155cace97
kkovati <kkovati@gmail.com>
2024-06-27 11:06:59 +0200

    TreePathCoder initial version

--- src/game_manager.hpp
+++ src/game_manager.hpp
@@ -1,5 +1,6 @@
 #include <optional>
 #include <tuple>
+#include <bitset>
 
 #include "game_rules.hpp"
 
@@ -20,6 +21,57 @@ private:
 	int n_minimax_call = 0;
 	int sum_time = 0;
 	int n_meas = 0;
+};
+
+
+class TreePathCoder1 {
+private:
+    uint64_t code1; // 16 digits stored in a 64-bit integer, 4 bits per digit
+    uint64_t code2; // 16 digits stored in another 64-bit integer, 4 bits per digit
+
+public:
+    TreePathCoder1(uint64_t initialCode1 = 0, uint64_t initialCode2 = 0) : code1(initialCode1), code2(initialCode2) {}
+
+    void setDigit(int position, uint8_t value) {
+        assert(position >= 0 && position <= 31 && value <= 9);
+        if (position < 16) {
+            code1 &= ~((uint64_t)0xF << (position * 4)); // Clear the bits at the specified position in code1
+            code1 |= ((uint64_t)value << (position * 4)); // Set the bits to the new value in code1
+        }
+        else {
+            position -= 16; // Adjust the position for code2
+            code2 &= ~((uint64_t)0xF << (position * 4)); // Clear the bits at the specified position in code2
+            code2 |= ((uint64_t)value << (position * 4)); // Set the bits to the new value in code2
+        }
+    }
+
+    uint8_t getDigit(int position) const {
+        assert(position >= 0 && position <= 31);
+        if (position < 16) {
+            return (code1 >> (position * 4)) & 0xF; // Extract the bits at the specified position in code1
+        }
+        else {
+            position -= 16; // Adjust the position for code2
+            return (code2 >> (position * 4)) & 0xF; // Extract the bits at the specified position in code2
+        }
+    }
+
+    void printCode() const {
+        std::cout << "Iterative" << std::endl;
+        for (int i = 31; i >= 0; --i) {
+            std::cout << unsigned(getDigit(i));
+        }
+        std::cout << std::endl;
+
+        std::cout << "Bitset" << std::endl;
+        std::bitset<64> x(code1);
+        std::cout << x << '\n';        
+        std::bitset<64> y(code2);
+        std::cout << y << '\n';
+        std::cout << std::endl;
+
+        //TODO: delete #include <bitset>
+    }
 };
 
 }
--- src/main.cpp
+++ src/main.cpp
@@ -5,9 +5,19 @@
 
 int main()
 {
-	std::cout << "Start simulation\n";
-	auto gameManager = ulti_minimax::GameManager();
-	gameManager.simulate();
+	//std::cout << "Start simulation\n";
+	//auto gameManager = ulti_minimax::GameManager();
+	//gameManager.simulate();
+	//return 0;
+
+	// TODO: tests needed
+	auto c1 = ulti_minimax::TreePathCoder1();
+	for (int i = 0; i < 32; ++i) {
+		c1.setDigit(i, i % 10);
+		std::cout << i << std::endl;
+		c1.printCode();
+	}
+	
 	return 0;
 }
 
